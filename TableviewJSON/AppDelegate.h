3245194c3e04ee79b7ef3026310253a609f04e02
Jon Toews <jtoews@Jons-iMac.hsd1.ca.comcast.net>
2015-08-11 13:23:31 -0700

    Initial Commit

---
+++ TableviewJSON/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TableviewJSON
+//
+//  Created by Jon Toews on 8/11/15.
+//  Copyright (c) 2015 Jon Toews. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ TableviewJSON/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  TableviewJSON
+//
+//  Created by Jon Toews on 8/11/15.
+//  Copyright (c) 2015 Jon Toews. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
